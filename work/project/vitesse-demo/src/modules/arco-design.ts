b02c277e77e8adf01e38c5db0be5321604e7a563
slc5514 <slc5514@163.com>
2021-12-13 14:59:46 +0800

    up

--- work/project/vitesse-demo/src/modules/arco-design.ts
+++ work/project/vitesse-demo/src/modules/arco-design.ts
@@ -1,9 +1,10 @@
 import ArcoVue from '@arco-design/web-vue'
+import { Plugin } from 'vue'
 import { UserModule } from '~/types'
 import '@arco-design/web-vue/dist/arco.css'
 
 export const install: UserModule = ({ app }) => {
-  app.use(ArcoVue)
+  app.use((ArcoVue as Plugin))
 }
 
 export default install
--- work/project/vitesse-demo/src/modules/pinia.ts
+++ work/project/vitesse-demo/src/modules/pinia.ts
@@ -1,3 +1,4 @@
+import { Plugin } from 'vue'
 import { createPinia } from 'pinia'
 import { UserModule } from '~/types'
 
@@ -5,7 +6,7 @@ import { UserModule } from '~/types'
 // https://pinia.esm.dev/
 export const install: UserModule = ({ isClient, initialState, app }) => {
   const pinia = createPinia()
-  app.use(pinia)
+  app.use((pinia as Plugin))
   // Refer to
   // https://github.com/antfu/vite-ssg/blob/main/README.md#state-serialization
   // for other serialization strategies.
